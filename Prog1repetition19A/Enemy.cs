1bb9381901a51cbbd9a0e8c9686e1fb90fdb3161
Mikael Bergström <krank23@gmail.com>
2021-08-25 18:13:54 +0200

    Initial commit

---
+++ Prog1repetition19A/Enemy.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Enemy
+{
+  public int x = 0;
+  public int y = 32;
+
+  public bool alive = true;
+
+  public int speed = 2;
+}
---
+++ Prog1repetition19A/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+Enemy goomba = new Enemy();
+
+Console.WriteLine($"Goombans X-värde är {goomba.x}");
+
+goomba.x += goomba.speed;
+
+
+// string teacherName = "";
+// DogeCoin();
+
+// static void DogeCoin()
+// {
+//   Console.WriteLine("Poof goes money");
+// }
+
+// int[] numbers = { 4, 5, 112, 89 };
+
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//   Console.WriteLine(numbers[i]);
+// }
+
+
+// while (teacherName != "Micke")
+// {
+//   System.Console.WriteLine("Vad heter du?");
+//   teacherName = Console.ReadLine();
+
+//   Console.WriteLine($"Hello {teacherName}!");
+
+//   if (teacherName == "Micke")
+//   {
+//     Console.WriteLine("yay!");
+//   }
+
+//   int age = 0;
+//   while (age <= 0 || age > 100)
+//   {
+//     Console.WriteLine("Ålder?");
+//     string ageS = Console.ReadLine();
+//     int.TryParse(ageS, out age);
+//   }
+
+// }
+
+
+Console.ReadLine();
